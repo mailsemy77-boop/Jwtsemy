7d61fd9fcb2321db87d1f8df80712358ed7f15d9
mailsemy77-boop <mailsemy77@gmail.com>
2026-09-19 16:12:51 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,13 @@
 import json
 import requests
 import sys
+import base64
+from datetime import datetime
 from flask import Flask, request, jsonify
 from Crypto.Cipher import AES
 from Crypto.Util.Padding import pad, unpad
 
 # -------------------- Include protobuf generated code --------------------
-# All protobuf classes are defined here (from MajoRLoGinrEq_pb2.py and MajoRLoGinrEs_pb2.py)
-
 from google.protobuf import descriptor as _descriptor
 from google.protobuf import descriptor_pool as _descriptor_pool
 from google.protobuf import symbol_database as _symbol_database
@@ -15,7 +15,7 @@ from google.protobuf.internal import builder as _builder
 
 _sym_db = _symbol_database.Default()
 
-# --- MajorLoginReq protobuf RIZER ---
+# --- MajorLoginReq protobuf ---
 DESCRIPTOR = _descriptor_pool.Default().AddSerializedFile(b'\n\x13MajorLoginReq.proto\"\xfa\n\n\nMajorLogin\x12\x12\n\nevent_time\x18\x03 \x01(\t\x12\x11\n\tgame_name\x18\x04 \x01(\t\x12\x13\n\x0bplatform_id\x18\x05 \x01(\x05\x12\x16\n\x0e\x63lient_version\x18\x07 \x01(\t\x12\x17\n\x0fsystem_software\x18\x08 \x01(\t\x12\x17\n\x0fsystem_hardware\x18\t \x01(\t\x12\x18\n\x10telecom_operator\x18\n \x01(\t\x12\x14\n\x0cnetwork_type\x18\x0b \x01(\t\x12\x14\n\x0cscreen_width\x18\x0c \x01(\r\x12\x15\n\rscreen_height\x18\r \x01(\r\x12\x12\n\nscreen_dpi\x18\x0e \x01(\t\x12\x19\n\x11processor_details\x18\x0f \x01(\t\x12\x0e\n\x06memory\x18\x10 \x01(\r\x12\x14\n\x0cgpu_renderer\x18\x11 \x01(\t\x12\x13\n\x0bgpu_version\x18\x12 \x01(\t\x12\x18\n\x10unique_device_id\x18\x13 \x01(\t\x12\x11\n\tclient_ip\x18\x14 \x01(\t\x12\x10\n\x08language\x18\x15 \x01(\t\x12\x0f\n\x07open_id\x18\x16 \x01(\t\x12\x14\n\x0copen_id_type\x18\x17 \x01(\t\x12\x13\n\x0b\x64\x65vice_type\x18\x18 \x01(\t\x12\'\n\x10memory_available\x18\x19 \x01(\x0b\x32\r.GameSecurity\x12\x14\n\x0c\x61\x63\x63\x65ss_token\x18\x1d \x01(\t\x12\x17\n\x0fplatform_sdk_id\x18\x1e \x01(\x05\x12\x1a\n\x12network_operator_a\x18) \x01(\t\x12\x16\n\x0enetwork_type_a\x18* \x01(\t\x12\x1c\n\x14\x63lient_using_version\x18\x39 \x01(\t\x12\x1e\n\x16\x65xternal_storage_total\x18< \x01(\x05\x12\"\n\x1a\x65xternal_storage_available\x18= \x01(\x05\x12\x1e\n\x16internal_storage_total\x18> \x01(\x05\x12\"\n\x1ainternal_storage_available\x18? \x01(\x05\x12#\n\x1bgame_disk_storage_available\x18@ \x01(\x05\x12\x1f\n\x17game_disk_storage_total\x18\x41 \x01(\x05\x12%\n\x1d\x65xternal_sdcard_avail_storage\x18\x42 \x01(\x05\x12%\n\x1d\x65xternal_sdcard_total_storage\x18\x43 \x01(\x05\x12\x10\n\x08login_by\x18I \x01(\x05\x12\x14\n\x0clibrary_path\x18J \x01(\t\x12\x12\n\nreg_avatar\x18L \x01(\x05\x12\x15\n\rlibrary_token\x18M \x01(\t\x12\x14\n\x0c\x63hannel_type\x18N \x01(\x05\x12\x10\n\x08\x63pu_type\x18O \x01(\x05\x12\x18\n\x10\x63pu_architecture\x18Q \x01(\t\x12\x1b\n\x13\x63lient_version_code\x18S \x01(\t\x12\x14\n\x0cgraphics_api\x18V \x01(\t\x12\x1d\n\x15supported_astc_bitset\x18W \x01(\r\x12\x1a\n\x12login_open_id_type\x18X \x01(\x05\x12\x18\n\x10\x61nalytics_detail\x18Y \x01(\x0c\x12\x14\n\x0cloading_time\x18\\ \x01(\r\x12\x17\n\x0frelease_channel\x18] \x01(\t\x12\x12\n\nextra_info\x18^ \x01(\t\x12 \n\x18\x61ndroid_engine_init_flag\x18_ \x01(\r\x12\x0f\n\x07if_push\x18\x61 \x01(\x05\x12\x0e\n\x06is_vpn\x18\x62 \x01(\x05\x12\x1c\n\x14origin_platform_type\x18\x63 \x01(\t\x12\x1d\n\x15primary_platform_type\x18\x64 \x01(\t\"5\n\x0cGameSecurity\x12\x0f\n\x07version\x18\x06 \x01(\x05\x12\x14\n\x0chidden_value\x18\x08 \x01(\x04\x62\x06proto3')
 
 _globals = globals()
@@ -30,7 +30,7 @@ if not _descriptor._USE_C_DESCRIPTORS:
 MajorLogin = _globals['MajorLogin']
 GameSecurity = _globals['GameSecurity']
 
-# --- MajorLoginRes protobuf (MajoRLoGinrEs_pb2) ---
+# --- MajorLoginRes protobuf ---
 DESCRIPTOR2 = _descriptor_pool.Default().AddSerializedFile(b'\n\x13MajorLoginRes.proto\"|\n\rMajorLoginRes\x12\x13\n\x0b\x61\x63\x63ount_uid\x18\x01 \x01(\x04\x12\x0e\n\x06region\x18\x02 \x01(\t\x12\r\n\x05token\x18\x08 \x01(\t\x12\x0b\n\x03url\x18\n \x01(\t\x12\x11\n\ttimestamp\x18\x15 \x01(\x03\x12\x0b\n\x03key\x18\x16 \x01(\x0c\x12\n\n\x02iv\x18\x17 \x01(\x0c\x62\x06proto3')
 _globals2 = globals()
 _builder.BuildMessageAndEnumDescriptors(DESCRIPTOR2, _globals2)
@@ -42,10 +42,17 @@ if not _descriptor._USE_C_DESCRIPTORS:
 MajorLoginRes = _globals2['MajorLoginRes']
 
 # -------------------- End protobuf includes --------------------
+
+# -------------------- OB55 CONFIG --------------------
+RELEASE_VERSION = "OB55"
+CLIENT_VERSION = "1.132.1"
+CLIENT_VERSION_CODE = "2024019028"   # OB55 version code
+# -----------------------------------------------------
+
 def get_garena_token(uid, password):
     url = "https://ffmconnect.live.gop.garenanow.com/oauth/guest/token/grant"
     headers = {
-        "User-Agent": "GarenaMSDK/4.0.19P4 (Vivo Y15c; Android 12; en;IN;)",
+        "User-Agent": "GarenaMSDK/4.0.19P9 (Vivo Y15c; Android 12; en;IN;)",
         "Content-Type": "application/x-www-form-urlencoded",
     }
     data = {
@@ -63,10 +70,11 @@ def get_garena_token(uid, password):
     except:
         pass
     return None
-    
+
+
 app = Flask(__name__)
 
-# AES constants (from xC4.py)
+# AES constants
 AES_KEY = bytes([89, 103, 38, 116, 99, 37, 68, 69, 117, 104, 54, 37, 90, 99, 94, 56])
 AES_IV = bytes([54, 111, 121, 90, 68, 114, 50, 50, 69, 51, 121, 99, 104, 106, 77, 37])
 
@@ -74,26 +82,27 @@ def encrypt_aes(data: bytes) -> bytes:
     cipher = AES.new(AES_KEY, AES.MODE_CBC, AES_IV)
     return cipher.encrypt(pad(data, AES.block_size))
 
+
 def build_major_login(open_id: str, access_token: str, platform_type: int) -> bytes:
     major = MajorLogin()
-    # OB55 updated values from payload
-    major.event_time = "2026-09-19 12:00:00"  # Updated for OB55
+    # OB55 updated values
+    major.event_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
     major.game_name = "free fire"
     major.platform_id = 1
-    major.client_version = "1.132.3"  # Changed: OB55 version
-    major.system_software = "Android OS 9 / API-28 (PI/rel.cjw.20220518.114133)"  # Updated
+    major.client_version = CLIENT_VERSION                 # OB55: 1.132.1
+    major.system_software = "Android OS 11 / API-30 (RKQ1.201112.002)"
     major.system_hardware = "Handheld"
-    major.telecom_operator = "MTN/Spacecetal"  # Updated
+    major.telecom_operator = "Verizon Wireless"
     major.network_type = "WIFI"
     major.screen_width = 1920
     major.screen_height = 1080
-    major.screen_dpi = "240"  # Updated
-    major.processor_details = "x86-64 SSE3 SSE4.1 SSE4.2 AVX AVX2 | 2400 | 4"  # Updated
-    major.memory = 7910  # Updated
+    major.screen_dpi = "240"
+    major.processor_details = "ARMv7 VFPv3 NEON VMH | 2400 | 4"
+    major.memory = 5951
     major.gpu_renderer = "Adreno (TM) 640"
-    major.gpu_version = "OpenGL ES 3.2"  # Updated
-    major.unique_device_id = "Google|625f7166-91a7-495b-9f16-08fe9d3c6533"  # Updated
-    major.client_ip = "176.28.139.185"  # Updated
+    major.gpu_version = "OpenGL ES 3.2"
+    major.unique_device_id = "Google|74b585a9-0268-4ad3-8f36-ef41d2e53610"
+    major.client_ip = "172.190.111.97"
     major.language = "en"
     major.open_id = open_id
     major.open_id_type = "4"
@@ -102,44 +111,45 @@ def build_major_login(open_id: str, access_token: str, platform_type: int) -> by
     major.memory_available.hidden_value = 81
     major.access_token = access_token
     major.platform_sdk_id = 1
-    major.network_operator_a = "MTN/Spacecetal"  # Updated
+    major.network_operator_a = "Verizon Wireless"
     major.network_type_a = "WIFI"
-    major.client_using_version = "1ac4b80ecf0478a44203bf8fac6120f5"  # Updated from payload
-    major.external_storage_total = 47091  # Updated
-    major.external_storage_available = 40784  # Updated
-    major.internal_storage_total = 40784  # Updated
-    major.internal_storage_available = 32080  # Updated
-    major.game_disk_storage_available = 47221  # Updated
-    major.game_disk_storage_total = 32080  # Updated
-    major.external_sdcard_avail_storage = 47221  # Updated
-    major.external_sdcard_total_storage = 32080  # Updated
+    major.client_using_version = "9c9e9d3e1e1e5b2b6b1e5a4a0a1a2b3c"   # OB55 placeholder hash
+    major.external_storage_total = 47091
+    major.external_storage_available = 40784
+    major.internal_storage_total = 40784
+    major.internal_storage_available = 32080
+    major.game_disk_storage_available = 47221
+    major.game_disk_storage_total = 32080
+    major.external_sdcard_avail_storage = 47221
+    major.external_sdcard_total_storage = 32080
     major.login_by = 3
-    major.library_path = "/data/app/com.dts.freefireth-fpXCSpHIV6dKC7jL-WOyRA==/lib/arm"  # Updated
+    major.library_path = "/data/app/com.dts.freefireth-fpXCSpHIV6dKC7jL-WOyRA==/lib/arm"
     major.reg_avatar = 1
-    major.library_token = "e62ab9354d8fb5fb081db338acb33491|/data/app/com.dts.freefireth-fpXCSpHIV6dKC7jL-WOyRA==/base.apk"  # Updated
+    major.library_token = "e62ab9354d8fb5fb081db338acb33491|/data/app/com.dts.freefireth-fpXCSpHIV6dKC7jL-WOyRA==/base.apk"
     major.channel_type = 3
     major.cpu_type = 2
-    major.cpu_architecture = "32"  # Updated
-    major.client_version_code = "2019119026"  # Updated
+    major.cpu_architecture = "32"
+    major.client_version_code = CLIENT_VERSION_CODE       # OB55 updated
     major.graphics_api = "OpenGLES2"
     major.supported_astc_bitset = 16383
     major.login_open_id_type = 4
-    major.analytics_detail = b"\x15\x12\x14\x50\x0e\x59\x03\x49\x51\x0e\x46\x09\x00\x11\x58\x43\x39\x5f\x00\x5b\x51\x0f\x68\x5b\x56\x0a\x61\x07\x57\x6d\x0f\x03\x66"  # Updated from payload
-    major.loading_time = 48862  # Updated
+    major.analytics_detail = b"\x15\x12\x14\x50\x0e\x59\x03\x49\x51\x0e\x46\x09\x00\x11\x58\x43\x39\x5f\x00\x5b\x51\x0f\x68\x5b\x56\x0a\x61\x07\x57\x6d\x0f\x03\x66"
+    major.loading_time = 48862
     major.release_channel = "android"
-    major.extra_info = "KqsHT8W93GdcG3ZozENfFwVHtm7qq1eRUNaIDNgRobozIBtLOiYCc4Y6zvvpcICxzQF2sOE4cbytwLs4xZbRnpRMpmWRQKmeO5vcs8nQYBhwqH7K"  # Updated
+    major.extra_info = "KqsHT8W93GdcG3ZozENfFwVHtm7qq1eRUNaIDNgRobozIBtLOiYCc4Y6zvvpcICxzQF2sOE4cbytwLs4xZbRnpRMpmWRQKmeO5vcs8nQYBhwqH7K"
     major.android_engine_init_flag = 110009
     major.if_push = 1
     major.is_vpn = 1
     major.origin_platform_type = str(platform_type)
     major.primary_platform_type = str(platform_type)
     return major.SerializeToString()
-#AUTO DETECTION BY RIZER
+
+
 def try_major_login(open_id: str, access_token: str, platform_type: int):
     payload = build_major_login(open_id, access_token, platform_type)
     encrypted_payload = encrypt_aes(payload)
 
-    url = "https://loginbp.ggblueshark.com/MajorLogin"
+    url = "https://loginbp.ppmainecoonghj.com/MajorLogin"
     headers = {
         "User-Agent": "Dalvik/2.1.0 (Linux; U; Android 11; ASUS_Z01QD Build/PI)",
         "Connection": "Keep-Alive",
@@ -147,7 +157,10 @@ def try_major_login(open_id: str, access_token: str, platform_type: int):
         "Content-Type": "application/x-www-form-urlencoded",
         "X-Unity-Version": "2018.4.11f1",
         "X-GA": "v1 1",
-        "ReleaseVersion": "OB55"  # Changed: OB54 -> OB55
+        "ReleaseVersion": RELEASE_VERSION,   # OB55
+        "X-Ga-Sv": "1789534056",
+        "PlAy_VeR": CLIENT_VERSION,
+        "Ob_VeR": RELEASE_VERSION,
     }
     try:
         resp = requests.post(url, data=encrypted_payload, headers=headers, verify=False, timeout=10)
@@ -169,8 +182,8 @@ def try_major_login(open_id: str, access_token: str, platform_type: int):
         print(f"MajorLogin error for platform {platform_type}: {e}")
     return None
 
+
 def decode_jwt(token: str) -> dict:
-    import base64
     parts = token.split('.')
     if len(parts) != 3:
         return {}
@@ -181,13 +194,13 @@ def decode_jwt(token: str) -> dict:
     except Exception:
         return {}
 
+
 @app.route('/token', methods=['GET'])
 def token_endpoint():
     access_token = request.args.get('access_token')
     uid = request.args.get('uid')
     password = request.args.get('password')
 
-    # Naya Part: Agar UID/Pass diya hai toh access_token fetch karo
     if uid and password:
         garena_res = get_garena_token(uid, password)
         if garena_res and 'access_token' in garena_res:
@@ -198,9 +211,7 @@ def token_endpoint():
     if not access_token:
         return jsonify({"error": "Missing access_token or uid/password"}), 400
 
-    # Baki ka script (Inspect aur try_major_login) yaha se continue hoga...
-
-    # Step 1: Get open_id from Garena inspect endpoint
+    # Step 1: Get open_id from inspect
     inspect_url = f"https://100067.connect.garena.com/oauth/token/inspect?token={access_token}"
     try:
         insp_resp = requests.get(inspect_url, timeout=10)
@@ -214,7 +225,7 @@ def token_endpoint():
         return jsonify({"error": f"Inspect request failed: {str(e)}"}), 500
 
     # Step 2: Try each platform type
-    platform_types = [2, 3, 4, 6, 8]
+    platform_types = [8, 3, 4, 6, 2]
     last_error = None
     for pt in platform_types:
         result = try_major_login(open_id, access_token, pt)
@@ -232,12 +243,13 @@ def token_endpoint():
             })
         else:
             last_error = f"Failed with platform_type {pt}"
-    # If we get here, all attempts failed
+
     return jsonify({
         "success": False,
         "error": "MajorLogin failed. Account may be banned, not registered, or token invalid.",
         "detail": last_error
     }), 401
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8006, debug=False)
